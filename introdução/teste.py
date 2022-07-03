c7778b5be420ea24e04ca043af799e915e70ac76
M-Dyeimison <marcosdyeimison@gmail.com>
2022-07-03 18:32:36 -0300

    initial commit

---
+++ introdução/teste.py
@@ -0,0 +1,20 @@
+numero = 2
+vetor = [2,3,4]
+import numpy as np
+vetor = np.asarray(vetor)
+vetor.max()
+vetor.argmax()
+vetor.min()
+vetor.mean()
+vetor.sum()
+
+vetor2 = 2*vetor
+np.arange(1, 100, 0.001)
+np.zeros(100)
+np.zeros((5,5))
+np.ones(10)
+np.ones((10,10))
+np.linspace(1, 10, 20)
+np.random.seed(1)
+np.random.randint(-1, 101, 100)
+np.random.randint(0,101,(10,10))
